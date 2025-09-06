dab7bd7cdc4e9a951533f33ab84e1f24ff099892
wk1817 <wangkai1817@gmail.com>
2025-09-06 14:54:24 +0800

    IHD-961454 update

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+from PyQt6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QAbstractItemView, QHeaderView
+from PyQt6 import uic
 
-import time
-import business.rcd as rcd
+import sys
 
-if __name__ == "__main__":
-    time.sleep(3)
-    hwnd = rcd.gtwh.get_Hwnd()
-    rcd.recording(hwnd)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = uic.loadUi("D:\project\9yinHs\9yinHs\9yinHs.ui")
+    table: QTableWidget = ui.tableWidget
+    table.setRowCount(6)
+    table.setColumnCount(7)
+    table.setHorizontalHeaderLabels(['编号', '句柄', '显示', '任务', '开始', '结束', '备注'])
+    for i in range(6):
+        for j in range(7):
+            data = QTableWidgetItem(str(i + j))
+            table.setItem(i, j, data)
+    table.setAlternatingRowColors(True)
+    table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+    table.horizontalHeader().setStretchLastSection(True)
+    table.horizontalHeader().setStyleSheet("QHeaderView::section{border-top: 1px solid #9370DB}")
+    ui.show()
+    sys.exit(app.exec())
